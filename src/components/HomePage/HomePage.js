7903ef0f53f7b65042c9c576a40e7aaadf4c699c
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-15 00:28:48 -0400

    fixing home page and adding some css

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./HomePage.css"
-import Ig from "./ig.PNG"
-import Github from "./github.PNG"
-import Phone from "./cellphone.PNG"
-import Email from "./email.PNG"
+import Ig from "./instagram.png"
+import Github from "./github.jpeg"
+import Phone from "./phone.jpeg"
+import Email from "./email.jpeg"
+import Linkedin from "./linkedin.png"
 
 const footerImg ={
     width: 40,
@@ -22,6 +23,7 @@ export default function HomePage() {
        <a href="https://github.com//kevinrhidalgo"><img style={footerImg} src={Github}/></a> 
         <a href="tel:555-555-5555"><img style={footerImg} src={Phone}/></a>
         <a href="mailto:EmailAddress@.XYZ.com"><img style={footerImg} src={Email}/></a>
+        <a href="https://www.linkedin.com/in/kevinrhidalgo/"><img style={footerImg} src={Linkedin}/></a>
         </div>
         </div>
         
