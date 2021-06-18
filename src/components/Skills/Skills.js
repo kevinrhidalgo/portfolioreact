1ac231a2d4065c633e563c79eb7b87feb5ea1ba7
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-17 21:32:29 -0400

    minor css adds

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -10,7 +10,8 @@ const myfoto={
     transform:'translate(400%,-120%)',
     width:'130px',
     height:'130px',
-    border:'beige solid 6px',
+    border:'#f0f0f0 solid 6px',
+    boxShadow: 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
 }
 const skillWord={
     display:'flex',
