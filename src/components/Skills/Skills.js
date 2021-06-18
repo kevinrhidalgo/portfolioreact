fbdec1c301898d58082584d79210c5fd43567325
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-17 21:23:20 -0400

    css for the repo button

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -25,13 +25,13 @@ const projImg={
 }
 const linkBtn={
     textDecoration:'none',
-    margin:'35px',
+    marginLeft:'90px',
     backgroundColor:'#fffaf1',
-    border:'rgb(91, 162, 255) solid 2px',
     color:'black',
     padding:'5px',
     textWeight:'800px',
     borderRadius:'10%',
+    boxShadow: 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
 }
 
 export default function Skills() {
@@ -179,24 +179,24 @@ export default function Skills() {
      <h1>Projects</h1>
 <div className='firstSection'>
  <div className='aSection'>
- <img style={projImg} src={shelfi}/> 
+ <a href="https://github.com/mascarelloa/shelfi"><img style={projImg} src={shelfi}/> </a> 
  <div className='descSec'>
  <h2>Shelfi.</h2>
        <p> A library application where you can search for your books of choice in which you want to read and plan to read.</p>
        <a style={linkBtn} href="https://github.com/mascarelloa/shelfi" class="card-link">Repo</a>
-       <a style={linkBtn} href="https://myshelfi.herokuapp.com/" class="card-link">App</a>
+       
 </div> 
 </div>    
 
 
 
 <div className='aSection'>
-<img style={projImg} src={salud}/>  
+<a href="https://myshelfi.herokuapp.com/"><img style={projImg} src={salud}/> </a>
 <div className='descSec'>
 <h2>Salud.</h2>
 <p>Salud is an application with the purpose of showing users that one can enjoy and have fun eating, all while eating clean.</p>
   <a style={linkBtn} href="https://github.com/mascarelloa/shelfi" class="card-link">Repo</a>
-       <a style={linkBtn} href="https://myshelfi.herokuapp.com/" class="card-link">App</a>
+       
 </div>
 
 </div> 
@@ -204,21 +204,19 @@ export default function Skills() {
 
 <div className='firstSection'>
 <div className='aSection'>
-<img style={projImg} src={underC}/>  
+<a href="https://github.com/kevinrhidalgo"> <img style={projImg} src={underC}/>  </a>
 <div className='descSec'>
 <h2>Coming Soon.</h2>
 <p>Project currently in coding development.</p>
        <a style={linkBtn} href="https://github.com/kevinrhidalgo" class="card-link">Repo</a>
-       <a style={linkBtn} href="https://github.com/kevinrhidalgo" class="card-link">App</a>
 </div>
 
 </div>
 <div className='aSection'>
-<img style={projImg} src={underC}/>  
+<a href="https://github.com/kevinrhidalgo"> <img style={projImg} src={underC}/>  </a> 
 <div className='descSec'>
 <h2>Coming Soon.</h2>
 <p>Project currently in coding development.</p>
-       <a style={linkBtn} href="https://github.com/kevinrhidalgo" class="card-link">Repo</a>
        <a style={linkBtn} href="https://github.com/kevinrhidalgo" class="card-link">App</a>
 </div>
 
