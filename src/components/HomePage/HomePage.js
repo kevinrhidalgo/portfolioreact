5969cd674cc4c85bd0ce136fd49eb1d53d426558
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-06-17 21:44:49 -0400

    css for the skills page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -11,7 +11,7 @@ const footerImg ={
     height: 50,
     borderRadius:'100%',
     margin: '12px',
-    opacity:'85%'
+    boxShadow: 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
     }
 
 export default function HomePage() {
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -18,7 +18,9 @@ const skillWord={
     justifyContent:'center',
     fontFamily: 'Anton, sans-serif',
     paddingTop: '30px',
-    color:'white'
+    marginRight:'20px',
+    color:'white',
+    boxShadow: 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
 }
 const projImg={
     width:'240px',
