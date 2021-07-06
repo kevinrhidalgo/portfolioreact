9af5f04e7c4a327f6a62ff959aae07affc661c53
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-07-06 18:47:43 -0400

    editing the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import { Modal } from '../Modal/Modal';
 import "./Footer.css";
 import { Link } from "react-router-dom";
+import Ig from "../Modal/ig.png"
+import Github from "../Modal/ghub.png"
+import Phone from "../Modal/cellphone.jpeg"
+import Email from "../Modal/Email.png"
+import Linkedin from "../Modal/in.png"
 
 const footerBg={
   backgroundColor:'white',
@@ -24,6 +29,14 @@ const letsTalk={
   fontSize:'50px',
   color:'#222222'
 }
+const footerImg ={
+  width: 20,
+  height: 30,
+  borderRadius:'100%',
+  margin: '12px',
+  filter: 'grayscale(50%)',
+  boxShadow: 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px'
+  }
 
 function Footer() {
   const [showModal, setShowModal] = useState(false);
@@ -55,12 +68,14 @@ function Footer() {
       <a onClick={openModal}><p style={footerInfo}>Contacts</p></a>
         <Modal showModal={showModal} setShowModal={setShowModal} />
 
-        
-
-    
-    
-  
    </div>
+   <div className="contactList">
+       <a href="https://www.instagram.com/"><img style={footerImg} src={Ig}/></a> 
+       <a href="https://github.com//kevinrhidalgo"><img style={footerImg} src={Github}/></a> 
+        <a href="tel:555-555-5555"><img style={footerImg} src={Phone}/></a>
+        <a href="mailto:EmailAddress@.XYZ.com"><img style={footerImg} src={Email}/></a>
+        <a href="https://www.linkedin.com/in/kevinrhidalgo/"><img style={footerImg} src={Linkedin}/></a>
+        </div>   
     </nav>
     </div>
   );
