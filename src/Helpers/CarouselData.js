12e5a4a81bec15cf8d4966395b2d319dc9c2f0a0
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-07-09 21:48:54 -0400

    adding a helpers page for the modals of salud

---
+++ src/Helpers/CarouselData.js
@@ -0,0 +1,32 @@
+import saludHome from '../Images/saludHome.png'
+import saludFilm from '../Images/saludFilm.png'
+import saludBlog from '../Images/saludBlog.png'
+import recipes from '../Images/recipes.png'
+import login from '../Images/login.png'
+
+
+export const images = [
+    { title: "Salud Home", 
+      subtitle: "Salud app homepage", 
+      img: saludHome },
+    {
+      title: "Salud Film",
+      subtitle: "Film page from the salud application",
+      img: saludFilm,
+    },
+    {
+      title: "Salud Blog",
+      subtitle: "Blog page from the salud application",
+      img: saludBlog,
+    },
+    {
+      title: "salud login",
+      subtitle: "login page from the salud app",
+      img:login
+    },
+    {
+        title: "salud recipes",
+        subtitle: "recipes page from the salud app",
+        img:recipes
+      }
+  ];
