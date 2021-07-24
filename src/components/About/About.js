a0c19988fd76a32ee1d78344e3b1884ff1478540
kevin hidalgo <kevinrhidalgo@yahoo.com>
2021-07-24 02:01:12 -0400

    minor css chnages

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -282,12 +282,12 @@ export default function About() {
 
 <div id="col-2">
 <h2 style={hello}>Hello</h2>
-
-       <p>If you are reading this than that means you are either grading my work or here to get to know me a little. My name is Kevin Hidalgo and I am a web developer in the making and a current full-time student 
-        at the UCF coding bootcamp. I was born and raised in the Bronx, New York until August of 2019, in which I moved to Orlando, Florida. </p>
-        <p>Growing up I always had two career paths I wanted to pursue, coder or cardiologist. Prior to deciding between the two, I chose to pursue a career in medicine. I got as far as my junior year in which it was than I made the difficult decision to 
-        change career paths due to some unfortunate events. Thanks to a friend, it was he who introduced me to the concept of a coding bootcamp to jumpstart my career in web development.
-        Though i am inexperienced, I have alot of aspirations to continue learning various languages while also building my own applications from my pocket of ideas that i believe has potential; Hey! it doesn't hurt to dream big.  </p>
+<br/>
+<p>
+My name is Kevin Hidalgo and I am a Web Developer. If you are reading this, than hopefully this means you may be potentially interested in bringing me in to be
+apart of your team, so i'd like to tell you a little about me.  
+</p>
+       
 </div>
 </div>
 </div>
